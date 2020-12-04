5331e393cd619475f657f3ce91eb47b5bf58741c
kimgunyoun <73735719+kimgunyoun@users.noreply.github.com>
2020-12-04 23:57:10 +0900

    울산페이 나이별 사용금액분석

--- ulsan_hw.py
+++ ulsan_hw.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #1.파일을 데이터프레임으로 변환 df에 저장
-df=pd.read_csv('2020년+1월_2월.csv')
+df=pd.read_csv('2020년 1월_2월.csv')
 print(df)
 print('-1'*100)
 print('\n')
@@ -32,9 +32,10 @@ print('-5'*100)
 print('\n')
 
 
-#6.고객생년월일별 남녀별 거래금액
-df1=df.pivot_table(values='거래금액', index='고객 생년월일', columns='고객 성별', aggfunc=sum)
-print(df1)
+#6.고객생년월일별 남녀거래금액평균
+grouped=df.groupby(['고객 생년월일', '고객 성별'])
+gdf=grouped.mean()
+print(gdf)
 print('-6'*100)
 print('\n')
 
@@ -47,44 +48,35 @@ print('\n')
 
 # 나이로 연령대 열 추가하기
 df['연령대']=np.trunc((df['나이'])/10)*10
-
-#def generation(x):
-#    return(int(age)+"대")
-#여기가 문제인데 아직 age값을 받지도 않은 이 generation함수가 이렇게될까요?
-#age=int(input())
-
 print(df)
 print('-8'*100)
 print('\n')
 
-#연령대별 거래금액, 각 연령대별 남녀 분리
-df_age=df.pivot_table(values='거래금액', index='연령대', columns='고객 성별', aggfunc=sum)
+#각 연령대별 남녀거래금액의 합과 평균
+df_age=df.pivot_table(values='거래금액', index='연령대', columns='고객 성별', aggfunc=['sum','mean']) #데이터집계함수
 print(df_age)
 print('-9'*100)
 print('\n')
 
-#남녀 옆에 총합을 구하고 싶은데...어떻게 할까요?열추가로....
+# 분석에 사용할 열 선택('고객 생년월일', '고객 성별', '나이', '연령대')
+ndf = df[['고객 생년월일', '고객 성별', '나이', '연령대','거래금액']]
+print(ndf.head())
+print('@'*100)
+print('\n')
 
-
-#각 연령대별 1인당 평균 거래금액
 
 #그래프그릴때 한글폰트지원
 import matplotlib.font_manager as fm
 font_name = fm.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
 plt.rc('font', family=font_name)
 
-#그래프 figure생성(항상 먼저 종이를 만들어야한다)
-fig = plt.figure(figsize=(20,5))
 
 #그래프 스타일 서식지정
 plt.style.use('ggplot')
 
-#x축 눈금 라벨 회전하기
-plt.xticks(size=10, rotation=75) #년도별로 묶은 자료가 있어야 년도별로 표시됨. 아직 못만듦
-
-#그래프 출력
-plt.plot(df1.index,df1.values,marker='.')
+#그래프 출력 그래프 figure생성(항상 먼저 종이를 만들어야한다)
+ndf.plot(kind='scatter', x='고객 생년월일', y='거래금액', c='red',s=10,figsize=(10,5))
 plt.show()
+plt.close()
 print('-10'*100)
 print('\n')
-
